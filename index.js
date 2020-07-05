b0b89bed14b6fcaf864342b8b1bbb1088370c827
Vadim Nazarov <vadim-nazarov-web@yandex.ru>
2020-07-05 13:51:02 +0300

    22

--- index.js
+++ index.js
@@ -26,33 +26,56 @@ class Todo {
         li.key = todo.key;
         li.insertAdjacentHTML('beforeend', `
             <span class="text-todo">${todo.value}</span>
+            <button class="todo-edit"></button>
             <div class="todo-buttons">
 				<button class="todo-remove"></button>
-				<button class="todo-complete"></button>
+                <button class="todo-complete"></button>
+                
 			</div>
         `);
-
         if(todo.completed) {
             this.todoCompleted.append(li);
         } else{
             this.todoList.append(li);
         }
+        li.addEventListener('click', this.anim);
+    }
+
+    async anim(e){
+        let target = e.target;
+        if(target.classList.contains('todo-remove')){
+            this.style.opacity = 0;
+        } else if(target.classList.contains('todo-complete')){
+            this.style.opacity = 0;
+            setTimeout(() => {this.style.opacity = 1;}, 500)
+        }
     }
 
     addTodo(e){
         e.preventDefault();
-        if(this.input.value.trim()){
-            const newTodo = {
-                value: this.input.value,
-                completed: false,
-                key: this.generateKey()
-            };
-            this.todoData.set(newTodo.key, newTodo);
-            this.render();
+        let copyTodo;
+        this.todoData.forEach(item => {
+            if(item.value === this.input.value.trim()){
+                copyTodo = true;
+            } 
+        });
+        if(!copyTodo){
+            if(this.input.value.trim()){
+                const newTodo = {
+                    value: this.input.value,
+                    completed: false,
+                    key: this.generateKey()
+                };
+                this.todoData.set(newTodo.key, newTodo);
+                this.render();
+                this.input.value = '';
+            } else {
+                alert('Невозможно ввести пустую строку!');
+                this.input.value = '';
+            }
+        } else{
             this.input.value = '';
-        } else {
-            alert('Невозможно ввести пустую строку!');
-            this.input.value = '';
+            alert('Это дело уже существует!');
         }
     }
 
@@ -60,36 +83,41 @@ class Todo {
         return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
     }
 
-    toLeftAnimation(target) {
-        const li = target.closest('.todo-item');
-        let timer;
-        const startTime = Date.now();
-        const _this = this;
-        function move(passedTime) {
-          li.style.left = '-' + (passedTime * 0.5 + '%');
-        }
-        function animation() {
-            timer = requestAnimationFrame(animation);
-            const passedTime = Date.now() - startTime;
-            if (passedTime >= 200) {
-            cancelAnimationFrame(timer);
-            _this.render();
-            return target;
-            }
-            move(passedTime);
-        }
-        timer = requestAnimationFrame(animation);
-        console.log(li);
-        
-    }
-
     handler(e){
         let target = e.target;
         if(target.classList.contains('todo-remove')){
-            this.deleteItem(target);
+            setTimeout(() => {this.deleteItem(target);}, 300);
         } else if(target.classList.contains('todo-complete')){
-            this.completedItem(target);
+            setTimeout(() => {this.completedItem(target);}, 300)
+        } else if(target.classList.contains('todo-edit')){
+            this.edit(target);
         }
+    }
+
+    edit(target){
+        const li = target.parentNode,
+            span = li.querySelector('span');
+        let form = `
+                <form>
+                    <input></input>
+                </form>
+            `;
+        span.style.display = 'none';
+        li.insertAdjacentHTML('afterbegin', form);
+        form = li.querySelector('form');
+        let input = form.querySelector('input');
+        input.value = span.textContent;
+        form.addEventListener('submit', event => {
+            event.preventDefault();
+            if(input.value.trim()){
+                this.todoData.forEach(item => {if(item.value === span.textContent) item.value = input.value});
+                span.textContent = input.value;
+                form.remove();
+                span.style.display = 'inline-block';
+                this.render();
+            }
+        });
+        
     }
 
     deleteItem(target){
@@ -98,7 +126,6 @@ class Todo {
             if(item.value === li.textContent.trim())
                 this.todoData.delete(item.key);
         });
-        this.toLeftAnimation(target);
         this.render();
     }
 
@@ -108,7 +135,6 @@ class Todo {
             if(item.value === li.textContent.trim())
                 item.completed = (item.completed) ? false : true;
         });
-        this.toLeftAnimation(target);
         this.render();
     }
 
@@ -116,6 +142,7 @@ class Todo {
         this.form.addEventListener('submit', this.addTodo.bind(this));
         this.todoList.addEventListener('click', this.handler.bind(this));
         this.todoCompleted.addEventListener('click', this.handler.bind(this));
+
         this.render();
     }
 }
